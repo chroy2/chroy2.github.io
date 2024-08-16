a5f53b9ee4a0748c3725b7cc41694632bdd48e79
T-Witmer <troykwitmer@gmail.com>
2024-08-16 17:04:08 -0400

    update

--- public/static/cor-animations.js
+++ public/static/cor-animations.js
@@ -61,7 +61,7 @@ window.addEventListener('load', function() {
             this.centerX = this.width * 0.5;
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5
-            this.y = 125
+            this.y = 100
             this.gap = 10;
             this.mouse = {
                 radius: 2500,
@@ -106,7 +106,7 @@ window.addEventListener('load', function() {
             this.centerX = this.width * 0.5;
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5
-            this.y = this.centerY - this.image.height * 0.5
+            this.y = 100
             this.init(context)
         }
         update(distance) {
