899477c90a835845985eb14b3c286c756533d2d4
chroy2 <troykwitmer@gmail.com>
2026-03-07 19:28:10 -0500

    Removing Twitter

--- public/static/cor-buttons.js
+++ public/static/cor-buttons.js
@@ -1,25 +1,27 @@
-$(document).on("click", "#color-mode-btn", function() {
-    const modes = ["dark", "light"]
-    var attribute = $("html").attr("data-bs-theme")
-    var index = ((modes.indexOf(attribute) === modes.length - 1) ? 0 : modes.indexOf(attribute) + 1);
-    $("html").attr("data-bs-theme", modes[index])
-})
-
-$(document).ready(function() {
-    let darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    if (darkThemeMq == true) {
-        $("html").attr("data-bs-theme", "dark")
-    } else {
-        $("html").attr("data-bs-theme", "light")
-    }
+$(document).on("click", "#color-mode-btn", function () {
+  const modes = ["dark", "light"];
+  var attribute = $("html").attr("data-bs-theme");
+  var index =
+    modes.indexOf(attribute) === modes.length - 1
+      ? 0
+      : modes.indexOf(attribute) + 1;
+  $("html").attr("data-bs-theme", modes[index]);
 });
 
-let darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)")
-darkThemeMq.addListener(e => {
-    if (e.matches) {
-        $("html").attr("data-bs-theme", "dark")
-    } else {
-        $("html").attr("data-bs-theme", "light")
-    }
+$(document).ready(function () {
+  let darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  if (darkThemeMq == true) {
+    $("html").attr("data-bs-theme", "dark");
+  } else {
+    $("html").attr("data-bs-theme", "light");
+  }
 });
 
+let darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+darkThemeMq.addListener((e) => {
+  if (e.matches) {
+    $("html").attr("data-bs-theme", "dark");
+  } else {
+    $("html").attr("data-bs-theme", "light");
+  }
+});
